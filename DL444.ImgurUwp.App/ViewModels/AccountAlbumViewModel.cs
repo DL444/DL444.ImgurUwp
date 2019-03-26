c1193f73dd39c020df1e1feba440c56da7069e79
David Lee <DL444@outlook.com>
2019-03-26 15:44:29 +0800

    Implement account album commands.

--- DL444.ImgurUwp.App/ViewModels/AccountAlbumViewModel.cs
+++ DL444.ImgurUwp.App/ViewModels/AccountAlbumViewModel.cs
@@ -43,7 +43,16 @@ namespace DL444.ImgurUwp.App.ViewModels
         public string Privacy => Album.Privacy;
         public int Views => Album.Views;
         public string Link => Album.Link;
-        public bool Favorite => Album.Favorite;
+        public bool Favorite
+        {
+            get => Album.Favorite;
+            set
+            {
+                Album.Favorite = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Favorite)));
+            }
+        }
+
         public bool Nsfw => Album.Nsfw == true;
         public int Order => Album.Order;
         public string DeleteHash => Album.DeleteHash;
@@ -73,6 +82,12 @@ namespace DL444.ImgurUwp.App.ViewModels
 
         public AsyncCommand<bool> DeleteAlbumCommand { get; private set; }
         public AsyncCommand<bool> GalleryRemoveCommand { get; private set; }
+        public AsyncCommand<bool> FavoriteItemCommand { get; private set; }
+        public Command ShareCommand { get; private set; }
+        public Command CopyUrlCommand { get; private set; }
+        //public Command DownloadCommand { get; private set; }
+        public AsyncCommand<object> OpenBrowserCommand { get; private set; }
+
         async Task<bool> DeleteAlbum()
         {
             if (InGallery) { return false; }
@@ -91,15 +106,68 @@ namespace DL444.ImgurUwp.App.ViewModels
         {
             return await ApiClient.Client.RemoveGalleryItemAsync(Id);
         }
+        async Task<bool> FavoriteItem()
+        {
+            bool result = await ApiClient.Client.FavoriteAlbumAsync(Id);
+            Favorite = result;
+            return result;
+        }
+        void Share()
+        {
+            var transferMgr = Windows.ApplicationModel.DataTransfer.DataTransferManager.GetForCurrentView();
+            transferMgr.DataRequested += TransferMgr_DataRequested;
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.ShowShareUI();
+        }
+        void CopyUrl()
+        {
+            var package = new Windows.ApplicationModel.DataTransfer.DataPackage();
+            package.SetText(Link);
+            Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(package);
+        }
+        async Task<object> Download()
+        {
+            // TODO: Implement
+
+            //var pictureLib = await Windows.Storage.StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures);
+            //var defaultFolder = pictureLib.SaveFolder;
+
+            //using (var imageStream = System.IO.WindowsRuntimeStreamExtensions.AsInputStream(await ApiClient.Client.DownloadMediaAsync(DisplayImage.Link)))
+            //{
+            //    string filename = DisplayImage.Link.Substring(DisplayImage.Link.LastIndexOf('/') + 1);
+            //    var file = await defaultFolder.CreateFileAsync(filename, Windows.Storage.CreationCollisionOption.GenerateUniqueName);
+            //    using (var fileStream = (await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite)))
+            //    {
+            //        await Windows.Storage.Streams.RandomAccessStream.CopyAndCloseAsync(imageStream, fileStream);
+            //    }
+            //}
+            return null;
+        }
+        async Task<object> OpenBrowser()
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(Link));
+            return null;
+        }
 
         public AccountAlbumViewModel()
         {
             DeleteAlbumCommand = new AsyncCommand<bool>(DeleteAlbum);
             GalleryRemoveCommand = new AsyncCommand<bool>(GalleryRemove);
+            FavoriteItemCommand = new AsyncCommand<bool>(FavoriteItem);
+            ShareCommand = new Command(Share);
+            CopyUrlCommand = new Command(CopyUrl);
+            //DownloadCommand = new Command(Download);
+            OpenBrowserCommand = new AsyncCommand<object>(OpenBrowser);
         }
         public AccountAlbumViewModel(Album album) : this()
         {
             Album = album;
+        }
+
+        private void TransferMgr_DataRequested(Windows.ApplicationModel.DataTransfer.DataTransferManager sender, Windows.ApplicationModel.DataTransfer.DataRequestedEventArgs args)
+        {
+            var request = args.Request;
+            request.Data.SetWebLink(new Uri(Link));
+            request.Data.Properties.Title = string.IsNullOrWhiteSpace(Title) ? "Take a look at this on Imgur!" : $"{Title} - Imgur";
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
